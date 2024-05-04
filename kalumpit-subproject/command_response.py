da970451a0a278a449424b168636906406d21069
GTC-Byzantine <118645189+GTC-Byzantine@users.noreply.github.com>
2024-05-04 23:32:31 +0800

    Update command_response.py

--- kalumpit-subproject/command_response.py
+++ kalumpit-subproject/command_response.py
@@ -1,9 +1,11 @@
+import json
 import sys
 import requests
 import time
 import os
 import shutil
 import psutil
+import zipfile
 
 count = 0
 for i in psutil.pids():
@@ -32,6 +34,38 @@ with open('storage.txt', 'r') as f:
     storage_path = f.readlines()[0]
 
 
+def support_gbk(zip_file: zipfile.ZipFile):
+    name_to_info = zip_file.NameToInfo
+    # copy map first
+    for name, info in name_to_info.copy().items():
+        real_name = name.encode('cp437').decode('gbk')
+        if real_name != name:
+            info.filename = real_name
+            del name_to_info[name]
+            name_to_info[real_name] = info
+    return zip_file
+
+
+def upload_pack(pack_name_e):
+    try:
+        zip_addr = 'packages/{}/{}.zip'.format(pack_name_e, pack_name_e)
+        p = requests.get(
+            'https://aceproj.gtcsst.org.cn/contents/packages/{}/{}.zip'.format(pack_name_e, pack_name_e))
+        if str(p) == '<Response [404]>':
+            return
+        with open(zip_addr, 'wb') as f:
+            f.write(p.content)
+        with support_gbk(zipfile.ZipFile(zip_addr)) as zf:
+            zf.extractall('packages/{}/{}'.format(pack_name_e, pack_name_e))
+        ver = requests.get('https://aceproj.gtcsst.org.cn/'
+                           'contents/packages/{}/config.txt'.format(pack_name_e))
+        with open('packages/{}/config.txt'.format(pack_name_e), 'wb') as f:
+            f.write(ver.content)
+        os.remove(zip_addr)
+    except requests.exceptions.ConnectionError:
+        return
+
+
 while True:
     try:
         r = requests.post(req_url + 'overall.php', data={'VALIDATE': 'GTC contre project'})
@@ -45,18 +79,17 @@ while True:
     command = r.text.split('\n')
     print(command)
     for one in command:
-        if len(one) == 0:
-            continue
-        if one[0] == 'r':
+        single = one.split(' ')
+        if single[0] == 'redownload':
             os.system('start updater.exe')
-        elif one[0] == 'c':
+        elif single[0] == 'clear':
             dirs = os.listdir(storage_path)
             for item in dirs:
                 if os.path.isfile(os.path.join(storage_path, item)):
                     os.remove(os.path.join(storage_path, item))
                 else:
                     shutil.rmtree(os.path.join(storage_path, item), ignore_errors=True)
-        elif one[0] == 'u':
+        elif single[0] == 'upload':
             target_path = one[7:]
             try:
                 temp_file = open(os.path.join(storage_path, target_path), 'rb')
@@ -79,8 +112,33 @@ while True:
             requests.post(req_url + 'upload.php', headers=header, files=file_object)
             time.sleep(1)
             temp_file.close()
-        elif one[0] == 's':
+        elif single[0] == 'shutdown':
             os.system('shutdown -s -t 0')
-        elif one[0] == 'l':
+        elif single[0] == 'lock':
             os.system('rundll32.exe user32.dll LockWorkStation')
-    time.sleep(5)
+        elif single[0] == 'pack':
+            if len(single) <= 1:
+                continue
+            pack_name = single[1]
+            if not os.path.exists('packages'):
+                os.mkdir('packages')
+            if not os.path.exists(os.path.join('packages', pack_name)):
+                os.makedirs(os.path.join('packages', pack_name, pack_name))
+                upload_pack(pack_name)
+            with open(f'packages/{pack_name}/config.txt') as f:
+                config = f.readlines()[0]
+            config = json.loads(config)
+            try:
+                r = requests.get(f'https://aceproj.gtcsst.org.cn/contents/packages/{pack_name}/config.txt')
+                config_cloud = json.loads(r.text)
+                if config_cloud['version'] != config['version']:
+                    shutil.rmtree(f'packages/{pack_name}')
+                    os.makedirs(os.path.join('packages', pack_name, pack_name))
+                    upload_pack(pack_name)
+            except requests.exceptions.ConnectionError:
+                pass
+            cwd = os.getcwd()
+            os.chdir(f'packages/{pack_name}/{pack_name}')
+            os.system(f'start {config["exe"]}')
+            os.chdir(cwd)
+    time.sleep(9)
